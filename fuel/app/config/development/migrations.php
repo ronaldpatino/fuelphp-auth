4412681da142cf86871df9779a3e7958129a403f
ronaldpatino@gmail.com <acdc1976>
2012-07-11 12:15:40 -0500

    Migracion para dimensiones

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -7,7 +7,11 @@ return array(
 			'default' => 
 			array(
 				0 => '001_create_articulos',
-				1 => '002_create_fotos'
+				1 => '002_create_fotos',
+				2 => '003_create_users',
+				3 => '004_create_users_2',
+				4 => '005_create_users_3',
+				5 => '006_create_dimensions',
 			),
 		),
 		'module' => 
