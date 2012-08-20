f50fae53152c846b34667be7955e0d6d208a27ae
ronaldpatino@gmail.com <acdc1976>
2012-08-20 13:30:59 -0500

    Agregado archivo de configuracion en contexto de fuelphp de phpthumb

---
+++ fuel/app/config/phpthumb.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'document_root' => 'c:\\wamp\\apache2\\htdocs',
+    'photos_path' => 'c:\\wamp\\apache2\\htdocs\\photos',
+    'photos_cache' => '\\photos\\cache'
+);
+
+/* End of file config.php */
