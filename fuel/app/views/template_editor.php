993563de5d4b5b7b3e4407913ac205dc94faa711
ronaldpatino@gmail.com <acdc1976>
2012-09-21 12:11:15 -0500

    Quitado pie de página con estadisticas

--- fuel/app/views/template_editor.php
+++ fuel/app/views/template_editor.php
@@ -121,7 +121,7 @@
 
     </div><!-- end content-->
     <footer>
-        <p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+        <p class="pull-right"></p>
         <p>
             &nbsp;
         </p>
--- fuel/app/views/welcome/hello.php
+++ fuel/app/views/welcome/hello.php
@@ -50,7 +50,7 @@
 			</div>
 		</div>
 		<footer>
-			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+			<p class="pull-right"></p>
 			<p>
 				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
 				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
