60687758443e954306a2088df4ccdc677cf55c1a
unknown <ba01000660@ba01000660.austro.grpfin>
2012-10-17 11:08:36 -0500

    Creada funcion img_watermark

--- fuel/app/classes/myhtml.php
+++ fuel/app/classes/myhtml.php
@@ -21,4 +21,19 @@ class Myhtml  extends \Fuel\Core\Html
         $attr['alt'] = (isset($attr['alt'])) ? $attr['alt'] : pathinfo($src, PATHINFO_FILENAME);
         return html_tag('img', $attr);
     }
+
+	public static function img_watermark($src, $attr = array())
+    {
+        if ( ! preg_match('#^(\w+://)# i', $src))
+        {
+            //$src = \Uri::base(false).$src;
+        }
+		
+		
+        $attr['src'] = '/gr/public/phpthumb/phpThumb.php?src=' . $src . '&amp;fltr[]=wmi|/gr/public/assets/img/watermark.png|*|40&amp;w=400&amp;zc=1&amp;q=30';
+        $attr['alt'] = (isset($attr['alt'])) ? $attr['alt'] : pathinfo($src, PATHINFO_FILENAME);
+        //return html_tag('img', $attr);
+		
+		return $attr['src'];
+    }	
 }
