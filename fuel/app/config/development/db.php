6af20286a2d77e0ca69ec04c8d65bb4368ea1d28
unknown <ba01000660@ba01000660.austro.grpfin>
2012-10-17 12:09:44 -0500

    Configuraciones alternativas para deploy

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -2,13 +2,25 @@
 /**
  * The development database settings.
  */
-
+/*
+Deploy
 return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=grafico',
 			'username'   => 'root',
 			'password'   => 'root',
+		),
+	),
+);
+*/
+
+return array(
+	'default' => array(
+		'connection'  => array(
+			'dsn'        => 'mysql:host=localhost;dbname=grafico',
+			'username'   => 'root',
+			'password'   => 'dio1976',
 		),
 	),
 );
--- fuel/app/config/phpthumb.php
+++ fuel/app/config/phpthumb.php
@@ -1,5 +1,6 @@
 <?php
-
+/*
+Deploy
 return array(
     'document_root' => '/Mercurio/',
     'photos_path' => '/Mercurio/photos',
@@ -7,6 +8,18 @@ return array(
     'galleryroot' => '/photos',
     'phpthumbroot'=> '/gr/public/phpthumb/',
     'thumb_size'    => 120
+	
+
 );
+*/
+return array(
+    'document_root' => 'c:\\wamp\\apache2\\htdocs',
+    'photos_path' => 'c:\\wamp\\apache2\\htdocs\\photos',
+    'photos_cache' => '\\photos\\cache',
+    'galleryroot' => '\\photos',
+    'phpthumbroot'=> '/gr/public/phpthumb/',
+    'thumb_size'    => 120
+);
+
 
 /* End of file config.php */
